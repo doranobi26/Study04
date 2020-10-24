23cd6ed75d11b92ba1ff551886f6668aff1ab004
立石　純也 <dorasmapnogizaka46@tateishijunyanoMacBook-Air.local>
2020-10-24 18:40:53 +0900

    java-practice

---
+++ Practice.java
@@ -0,0 +1,23 @@
+class Practice {
+  public static void main(String[] args) {
+
+    String group = "乃木坂４６";
+
+    String name  = "白石麻衣";
+
+    int age = 28;
+
+    double height =162.0;
+
+    double weight = 46.5;
+
+    double bmi = weight/height*height;
+
+    System.out.println("所属グループは"+group+"です");
+    System.out.println("名前は"+name+"です");
+    System.out.println("年齢は"+age+"歳です");
+    System.out.println("身長は"+height+"mです");
+    System.out.println("体重は"+weight+"kgです");
+    System.out.println("BMIは"+bmi+"です");
+  }
+}
